778d70a24cd0f8518695fe4762efa32e291fac71
stasyukhymenko <yukhymenko.stas@gmail.com>
2024-11-22 23:56:19 +0200

    Fix spacing

--- src/components/Support/Support.tsx
+++ src/components/Support/Support.tsx
@@ -1,33 +1,33 @@
 import './Support.css';
 
 export const Support: React.FC = () => {
-    return (
-        <div className="support-background">
-            <div className="support-back">
-                <div className="question-background">
-                    <p className="question">Виникли проблеми або питання?</p>
-                </div>
-                <form action="" className="input-form">
-                    <label htmlFor="email" className="email-label">
-                        Ваш E-mail
-                    </label>
-                    <input
-                        type="email"
-                        id="email"
-                        className="email-input"
-                        placeholder="example@email.com"
-                    />
-                    <label htmlFor="problem-textarea" className="problem-label">
-                        Проблема або питання
-                    </label>
-                    <textarea
-                        id="problem-textarea"
-                        className="problem-textarea"
-                        placeholder="Опишіть свою проблему"
-                    ></textarea>
-                    <button className="send-button">Відправити</button>
-                </form>
-            </div>
+  return (
+    <div className="support-background">
+      <div className="support-back">
+        <div className="question-background">
+          <p className="question">Виникли проблеми або питання?</p>
         </div>
-    );
+        <form action="" className="input-form">
+          <label htmlFor="email" className="email-label">
+            Ваш E-mail
+          </label>
+          <input
+            type="email"
+            id="email"
+            className="email-input"
+            placeholder="example@email.com"
+          />
+          <label htmlFor="problem-textarea" className="problem-label">
+            Проблема або питання
+          </label>
+          <textarea
+            id="problem-textarea"
+            className="problem-textarea"
+            placeholder="Опишіть свою проблему"
+          ></textarea>
+          <button className="send-button">Відправити</button>
+        </form>
+      </div>
+    </div>
+  );
 };
