220a105a6e4f03bee8d1abc97d8cbe04042923fc
Illya <katinskyi.illia@lll.kpi.ua>
2024-11-23 21:15:40 +0200

    Implement conditional rendering for sign-up/log-in

--- src/components/RegisterPage/RegisterPage.tsx
+++ src/components/RegisterPage/RegisterPage.tsx
@@ -1,24 +1,51 @@
+import { useState } from 'react';
+import classNames from 'classnames';
 import './RegisterPage.css';
 
 export const RegisterPage: React.FC = () => {
+  const [isSignedUp, setIsSignedUp] = useState(false);
+
   return (
     <div className="register-block">
-      <div className="authentication-block">
+      {isSignedUp && <img src="/images/ecoflow.png" alt="EcoFlow" />}
+      <div
+        className={`authentication-block ${
+          isSignedUp ? 'log-in-block' : 'sign-up-block'
+        }`}
+      >
         <div className="authentication-buttons">
-          <button className="sign-up-button auth-button is-btn-active">
+          <button
+            className={classNames('sign-up-button', 'auth-button', {
+              'is-btn-active': !isSignedUp,
+            })}
+            onClick={() => setIsSignedUp(false)}
+          >
             Sign-up
           </button>
-          <button className="log-in-button auth-button">Log-in</button>
+          <button
+            className={classNames('log-in-button', 'auth-button', {
+              'is-btn-active': isSignedUp,
+            })}
+            onClick={() => setIsSignedUp(true)}
+          >
+            Log-in
+          </button>
         </div>
         <form className="register-form">
-          <div className="fullname-block">
-            <input type="text" placeholder="Ім'я" className="name auth-input" />
-            <input
-              type="text"
-              placeholder="Прізвище"
-              className="surname auth-input"
-            />
-          </div>
+          {!isSignedUp && (
+            <div className="fullname-block">
+              <input
+                type="text"
+                placeholder="Ім'я"
+                className="name auth-input"
+              />
+              <input
+                type="text"
+                placeholder="Прізвище"
+                className="surname auth-input"
+              />
+            </div>
+          )}
           <input
             type="email"
             placeholder="E-mail"
@@ -29,17 +56,26 @@ export const RegisterPage: React.FC = () => {
             placeholder="Пароль"
             className="password auth-input"
           />
-          <input
-            type="password"
-            placeholder="Підтвердити пароль"
-            className="repeat-password auth-input"
-          />
-          <button type="submit" className="register-button">
-            Зареєструватись
-          </button>
+          {!isSignedUp && (
+            <>
+              <input
+                type="password"
+                placeholder="Підтвердити пароль"
+                className="repeat-password auth-input"
+              />
+              <button type="submit" className="register-button">
+                Зареєструватись
+              </button>
+            </>
+          )}
+          {isSignedUp && (
+            <button type="submit" className="register-button">
+              Увійти
+            </button>
+          )}
         </form>
       </div>
-      <img src="/images/ecoflow.png" alt="EcoFlow" />
+      {!isSignedUp && <img src="/images/ecoflow.png" alt="EcoFlow" />}
     </div>
   );
 };
