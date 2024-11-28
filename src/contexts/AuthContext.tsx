e686d805490489079900c9b1636436a7b459c8ff
Illya <katinskyi.illia@lll.kpi.ua>
2024-11-29 01:23:01 +0200

    Add login and registration features

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 
 export const AuthContext = React.createContext({
   authorized: false,
-  login: (_email: string, _password: string) => false as boolean,
+  login: (_email: string, _password: string) => Promise.resolve(),
+  register: (
+    _name: string,
+    _surname: string,
+    _email: string,
+    _password: string
+  ) => Promise.resolve(),
 });
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,24 +1,56 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
 
 type Props = {
   children: React.ReactNode;
 };
 
+const BASE_URL = 'http://localhost:3000';
+
 export const AuthProvider: React.FC<Props> = ({ children }) => {
   const [authorized, setAuthorized] = useState(false);
 
-  const login = (email: string, password: string) => {
-    if (email && password) {
+  useEffect(() => {
+    const token = localStorage.getItem('jwt');
+    if (token) {
       setAuthorized(true);
-      return true;
     }
+  }, []);
 
-    return false;
+  const login = async (email: string, password: string) => {
+    const response = await fetch(BASE_URL + '/api/auth/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ email, password }),
+    });
+
+    if (!response.ok) throw new Error('Invalid email or password!');
+
+    const { token } = await response.json();
+
+    localStorage.setItem('jwt', token);
+    setAuthorized(true);
+  };
+
+  const register = async (
+    name: string,
+    surname: string,
+    email: string,
+    password: string
+  ) => {
+    const response = await fetch(BASE_URL + '/api/auth/register', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ name, surname, email, password }),
+    });
+
+    if (!response.ok) throw new Error('Registration failed!');
+
+    await login(email, password);
   };
 
   return (
-    <AuthContext.Provider value={{ authorized, login }}>
+    <AuthContext.Provider value={{ authorized, login, register }}>
       {children}
     </AuthContext.Provider>
   );
