682c58bd8049eb0e79715096aef9089bf60172f3
Illya <katinskyi.illia@lll.kpi.ua>
2024-12-02 01:49:05 +0200

    Fix styles of device card

--- src/components/DeviceCard/DeviceCard.tsx
+++ src/components/DeviceCard/DeviceCard.tsx
@@ -8,10 +8,10 @@ export const DeviceCard: React.FC = () => {
         src="https://avatars.akamai.steamstatic.com/adeb470d4165233694b0640595999b5764a7f4d1_full.jpg"
       />
       <div className="device-card-description-block">
-        <div className="device-card-title">Екофлоу від квадробера</div>
-        <div className="bottom-block">
-          <div className="city-name">м. Полтава</div>
-          <div className="price-value">500 грн/день</div>
+        <h2 className="device-card-title">Екофлоу від квадробера</h2>
+        <div className="device-info-block">
+          <p className="device-location">м. Полтава</p>
+          <p className="device-rent-price">500 грн/день</p>
         </div>
       </div>
     </div>
