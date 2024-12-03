ca742d986f170de03220cbc5f55b5f75b10a4bfd
Illya <katinskyi.illia@lll.kpi.ua>
2024-12-03 20:31:15 +0200

    Fix styles and integrate page in website

--- src/components/Payment/Payment.tsx
+++ src/components/Payment/Payment.tsx
@@ -2,58 +2,75 @@ import './Payment.css';
 
 export const Payment: React.FC = () => {
   return (
-    <div className="extra-container">
-      <div className="external-container">
-        <div className="cards-block">
-          <div className="gray-card">
-            <div className="yellow-chip"></div>
-            <div className="card-info-block">
-              <div className="left-block">
-                <div className="card-number-block">
-                  <p className="card-info-text big-input-label">Номер картки</p>
-                  <input
-                    className="big-input"
-                    placeholder="4441 8034 1488 2167"
-                  />
-                </div>
-                <div className="owner-name-block">
-                  <p className="card-info-text big-input-label owner-label">
-                    Ім'я власника
-                  </p>
-                  <input
-                    className="big-input"
-                    placeholder="Stanislav Yukhymenko"
-                  />
-                </div>
+    <>
+      <div className="payment-cards-block">
+        <div className="gray-card">
+          <div className="yellow-chip"></div>
+          <div className="payment-card-info-block">
+            <div className="payment-card-left-block">
+              <div className="payment-card-input-block">
+                <label
+                  htmlFor="cardNumberInput"
+                  className="payment-card-label main-label"
+                >
+                  Номер картки
+                </label>
+                <input
+                  id="cardNumberInput"
+                  className="payment-card-input payment-card-big-input info-input"
+                  placeholder="4441 8034 1488 2167"
+                />
               </div>
-              <div className="right-block">
-                <div className="expiration-date-block">
-                  <p className="card-info-text">Місяць/Рік</p>
-                  <input
-                    className="small-input expiration-date"
-                    placeholder="09/26"
-                  />
-                </div>
+              <div className="payment-card-input-block">
+                <label
+                  htmlFor="ownerNameInput"
+                  className="payment-card-label main-label"
+                >
+                  Ім'я власника
+                </label>
+                <input
+                  id="ownerNameInput"
+                  className="payment-card-input payment-card-big-input info-input"
+                  placeholder="Taras Shevchenko"
+                />
               </div>
             </div>
-          </div>
-          <div className="red-card">
-            <div className="black-line"></div>
-            <div className="cvv-block">
-              <p className="cvv-label card-info-text">CVV</p>
-              <input
-                className="small-input cvv-input"
-                type="text"
-                placeholder="328"
-              />
+            <div className="payment-card-right-block">
+              <div className="payment-card-input-block">
+                <label
+                  htmlFor="expirationDateInput"
+                  className="payment-card-label main-label"
+                >
+                  Місяць/Рік
+                </label>
+                <input
+                  id="expirationDateInput"
+                  className="payment-card-input payment-card-small-input info-input"
+                  placeholder="09/26"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div className="button-block">
-          <button className="cancel-button">Скасувати</button>
-          <button className="apply-button">Зберегти</button>
+        <div className="red-card">
+          <div className="black-line"></div>
+          <div className="payment-card-input-block cvv-block">
+            <label htmlFor="cvvInput" className="payment-card-label main-label">
+              CVV
+            </label>
+            <input
+              id="cvvInput"
+              className="payment-card-input payment-card-small-input info-input"
+              type="text"
+              placeholder="328"
+            />
+          </div>
         </div>
       </div>
-    </div>
+      <div className="edit-buttons-block">
+        <button className="cancel-button secondary-button">Скасувати</button>
+        <button className="save-button main-button">Зберегти</button>
+      </div>
+    </>
   );
 };
