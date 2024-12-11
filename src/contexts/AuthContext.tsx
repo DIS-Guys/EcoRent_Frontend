7616743cb6f7269c26f493813128d3de8ff971fe
Illya <katinskyi.illia@lll.kpi.ua>
2024-12-11 18:08:36 +0200

    Move auth logic to AuthProvider

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 
 export interface AuthContextProps {
   authorized: boolean;
-  setAuthorized: (authorized: boolean) => void;
+  login: (email: string, password: string) => void;
+  register: (
+    name: string,
+    surname: string,
+    email: string,
+    password: string
+  ) => void;
 }
 
 export const AuthContext = React.createContext<AuthContextProps | null>(null);
--- src/providers/AuthProvider.tsx
+++ src/providers/AuthProvider.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState } from 'react';
 import { AuthContext } from '../contexts/AuthContext';
+import { createUser, loginUser } from '../api/users';
+import { useLocation, useNavigate } from 'react-router-dom';
 
 type Props = {
   children: React.ReactNode;
@@ -7,6 +9,8 @@ type Props = {
 
 export const AuthProvider: React.FC<Props> = ({ children }) => {
   const [authorized, setAuthorized] = useState(false);
+  const navigate = useNavigate();
+  const { state } = useLocation();
 
   useEffect(() => {
     const token = localStorage.getItem('jwt');
@@ -15,8 +19,33 @@ export const AuthProvider: React.FC<Props> = ({ children }) => {
     }
   }, []);
 
+  const login = async (email: string, password: string) => {
+    try {
+      const { token } = await loginUser({ email, password });
+      localStorage.setItem('jwt', token);
+      setAuthorized(true);
+      navigate(state?.pathname || '/', { replace: true });
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      } else {
+        console.error('Unknown error:', error);
+      }
+    }
+  };
+
+  const register = async (
+    name: string,
+    surname: string,
+    email: string,
+    password: string
+  ) => {
+    await createUser({ name, surname, email, password });
+    await login(email, password);
+  };
+
   return (
-    <AuthContext.Provider value={{ authorized, setAuthorized }}>
+    <AuthContext.Provider value={{ authorized, login, register }}>
       {children}
     </AuthContext.Provider>
   );
