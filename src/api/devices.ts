f12a32fac8c8280a6a6ddbbc4994e1b838e3d221
Illya <katinskyi.illia@lll.kpi.ua>
2024-12-12 03:07:03 +0200

    Add feature to post own device

--- src/api/devices.ts
+++ src/api/devices.ts
@@ -4,3 +4,7 @@ import { client } from '../utils/sendRequest';
 export const getAllDevices = async () => {
   return client.get<Device[]>('/api/devices/getAllDevices');
 };
+
+export const postDevice = async (deviceInfo: FormData) => {
+  return client.post('/api/devices/addDevice', deviceInfo);
+};
