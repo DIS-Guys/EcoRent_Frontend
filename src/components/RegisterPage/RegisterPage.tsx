dd24043862e86dc65e355c6163388f1a32b0e517
Danil Kobzev <denthenttg@gmail.com>
2024-12-13 08:04:46 +0200

    add validation for register page

--- src/components/RegisterPage/RegisterPage.tsx
+++ src/components/RegisterPage/RegisterPage.tsx
@@ -2,6 +2,8 @@ import React, { useContext, useState } from 'react';
 import classNames from 'classnames';
 import './RegisterPage.css';
 import { AuthContext, AuthContextProps } from '../../contexts/AuthContext';
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
 
 export const RegisterPage: React.FC = () => {
   const [isSignedUp, setIsSignedUp] = useState(false);
@@ -12,13 +14,69 @@ export const RegisterPage: React.FC = () => {
   const [repeatPassword, setRepeatPassword] = useState('');
   const { login, register } = useContext(AuthContext) as AuthContextProps;
 
+  const validateRegisterFields = () => {
+    const errors = [];
+
+    if (!isSignedUp && !name.trim()) errors.push("Ім'я обов'язкове.");
+    if (!isSignedUp && !surname.trim()) errors.push("Прізвище обов'язкове.");
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) errors.push('Невірний формат e-mail.');
+
+    if (password.length < 6) {
+      errors.push('Пароль має містити щонайменше 6 символів.');
+    }
+
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d).+$/;
+    if (!passwordRegex.test(password)) {
+      errors.push('Пароль має містити щонайменше одну літеру та одну цифру.');
+    }
+
+    if (!isSignedUp && password !== repeatPassword) {
+      errors.push('Паролі не співпадають.');
+    }
+
+    if (errors.length > 0) {
+      toast.error(`Поля заповнені невірно: ${errors.join("\n")}`, {
+        position: 'bottom-right',
+      });
+      return false;
+    }
+
+    toast.success('Валідація пройшла успішно!', {
+      position: 'bottom-right',
+    });
+    return true;
+  };
+
+  const validateLoginFields = () => {
+    const errors = [];
+
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) errors.push('Невірний формат e-mail.');
+
+    if (errors.length > 0) {
+      toast.error(`Поля заповнені невірно: ${errors.join("\n")}`, {
+        position: 'bottom-right',
+      });
+      return false;
+    }
+
+    toast.success('Валідація пройшла успішно!', {
+      position: 'bottom-right',
+    });
+    return true;
+  }
+
   const handleLogin = (event: React.FormEvent) => {
     event.preventDefault();
+    if (!validateLoginFields()) return;
     login(email, password);
   };
 
   const handleRegister = (event: React.FormEvent) => {
     event.preventDefault();
+    if (!validateRegisterFields()) return;
     register(name, surname, email, password);
   };
 
