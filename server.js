a9dc79ef983b07026c68bdc8067667a1e2a0a461
Illya <katinskyi.illia@lll.kpi.ua>
2024-12-23 18:44:30 +0200

    Add server for deployment

---
+++ server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import path from 'path';
+
+const app = express();
+const port = 8080;
+const __dirname = path.resolve();
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
