2b905969d7ad3219a245a12f89e482fe998df737
stasyukhymenko <yukhymenko.stas@gmail.com>
2024-12-29 21:21:36 +0200

    Refactor and improve register and login test logic

--- tests/e2e/register-login.spec.ts
+++ tests/e2e/register-login.spec.ts
@@ -1,93 +1,23 @@
 import { test, expect } from '@playwright/test';
-import { faker } from '@faker-js/faker';
-import { log } from 'console';
+import {
+  generateRandomUser,
+  registerAndLogin,
+  deleteAccount,
+} from '../e2e/test-helper';
 
-const generatePassword = () => {
-  let password = '';
-  while (!/\d/.test(password)) {
-    password = faker.internet.password({
-      length: 10,
-      pattern: /[A-Za-z0-9]/,
-    });
-  }
-  return password;
-};
-
-const generateRandomUser = () => {
-  return {
-    name: faker.person.firstName(),
-    surname: faker.person.lastName(),
-    email: faker.internet.email(),
-    password: generatePassword(),
-  };
-};
-
-test.describe('User Registration and Login', () => {
-  let userData: {
-    name: string;
-    surname: string;
-    email: string;
-    password: string;
-  };
-
-  test.beforeAll(() => {
-    userData = generateRandomUser();
-  });
-
+test.describe('User Registration', () => {
   test('should register a new user', async ({ page }) => {
-    await page.waitForTimeout(1000);
-    await page.goto('http://localhost:5173/personal-page/cabinet/profile');
-
-    await page.fill('input[placeholder="Ім\'я"]', userData.name);
-    await page.fill('input[placeholder="Прізвище"]', userData.surname);
-    await page.fill('input[placeholder="E-mail"]', userData.email);
-    await page.fill('input[placeholder="Пароль"]', userData.password);
-    await page.fill(
-      'input[placeholder="Підтвердити пароль"]',
-      userData.password
-    );
-
-    await page.click('button[type="submit"]');
-
+    const userData = generateRandomUser();
+    await registerAndLogin(page, userData);
     await expect(page).toHaveURL(
       'http://localhost:5173/personal-page/cabinet/profile'
     );
 
-    const logoutButton = page.locator('.logout-button');
-    await logoutButton.click();
-
-    await expect(page).toHaveURL('http://localhost:5173/login');
+    await deleteAccount(page);
   });
 
-  test('should login with the registered user', async ({ page }) => {
-    await page.waitForTimeout(1000);
-    await page.goto('http://localhost:5173/personal-page/cabinet/profile');
-
-    const loginButton = page.locator('text=Log-in');
-    await loginButton.click();
-    await page.waitForTimeout(1000);
-
-    await page.fill('input[type="email"]', userData.email);
-    await page.fill('input[type="password"]', userData.password);
-
-    await page.click('button[type="submit"]');
-
-    await page.waitForTimeout(1000);
-    await expect(page).toHaveURL(
-      'http://localhost:5173/personal-page/cabinet/profile'
-    );
-
-    await page.waitForTimeout(1000);
-    const logoutButton = page.locator('.logout-button');
-    await logoutButton.click();
-
-    await expect(page).toHaveURL('http://localhost:5173/login');
-  });
-
-  test('should show error for invalid email format during registration', async ({
-    page,
-  }) => {
-    await page.waitForTimeout(1000);
+  test('should show error for invalid email format', async ({ page }) => {
+    const userData = generateRandomUser();
     await page.goto('http://localhost:5173/personal-page/cabinet/profile');
 
     await page.fill('input[placeholder="Ім\'я"]', userData.name);
@@ -106,20 +36,15 @@ test.describe('User Registration and Login', () => {
     await expect(errorToast).toHaveText('Невірний формат e-mail.');
   });
 
-  test('should show error for mismatched passwords during registration', async ({
-    page,
-  }) => {
-    await page.waitForTimeout(1000);
+  test('should show error for mismatched passwords', async ({ page }) => {
+    const userData = generateRandomUser();
     await page.goto('http://localhost:5173/personal-page/cabinet/profile');
 
     await page.fill('input[placeholder="Ім\'я"]', userData.name);
     await page.fill('input[placeholder="Прізвище"]', userData.surname);
     await page.fill('input[placeholder="E-mail"]', userData.email);
     await page.fill('input[placeholder="Пароль"]', userData.password);
-    await page.fill(
-      'input[placeholder="Підтвердити пароль"]',
-      'differentPassword'
-    );
+    await page.fill('input[placeholder="Підтвердити пароль"]', 'different');
 
     await page.click('button[type="submit"]');
 
@@ -128,10 +53,8 @@ test.describe('User Registration and Login', () => {
     await expect(errorToast).toHaveText('Паролі не збігаються.');
   });
 
-  test('should show error for short password during registration', async ({
-    page,
-  }) => {
-    await page.waitForTimeout(1000);
+  test('should show error for short password', async ({ page }) => {
+    const userData = generateRandomUser();
     await page.goto('http://localhost:5173/personal-page/cabinet/profile');
 
     await page.fill('input[placeholder="Ім\'я"]', userData.name);
@@ -144,7 +67,6 @@ test.describe('User Registration and Login', () => {
 
     const errorToasts = page.locator('.Toastify__toast--error');
     await expect(errorToasts).toHaveCount(2);
-
     await expect(errorToasts.nth(0)).toHaveText(
       'Пароль має містити щонайменше 6 символів.'
     );
